a887bc44d4f101499760ba5106a1b3400a2e048f
Arne Hormann <arnehormann@gmail.com>
2014-10-08 23:56:58 +0200

    first release, missing tests and examples

---
+++ errors.go
@@ -0,0 +1,24 @@
+package envflag
+
+type errors struct {
+	errs []error
+}
+
+func (e *errors) add(err error) {
+	if err == nil {
+		return
+	}
+	e.errs = append(e.errs, err)
+}
+
+func (e *errors) has() bool {
+	return len(e.errs) > 0
+}
+
+func (e *errors) get() error {
+	msgs := make([]string, len(e.errs))
+	for i, err := range e.errs {
+		msgs[i] = err.Error()
+	}
+	return fmt.Errorf(strings.Join(msgs, "\n"))
+}
